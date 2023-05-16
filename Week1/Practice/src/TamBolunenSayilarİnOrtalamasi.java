64a1014df6d7104353dddcbd1d4011a6f4ca211e
Muhammed Said Bayri <m.baayri@gmail.com>
2023-05-16 16:53:54 +0300

    3 ve 4'e tam bölünen sayıların ortalaması

---
+++ Week1/Practice/src/TamBolunenSayilarİnOrtalamasi.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TamBolunenSayilarİnOrtalamasi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int k, index = 0, average = 0;
+
+        System.out.print("Sayı Giriniz : ");
+        k = scanner.nextInt();
+
+        for (int i = 1; i < k; i++) {
+            if (i % 3 == 0 && i % 4 == 0){
+                average += i;
+                index += 1;
+            }
+        }
+
+        if (index == 0){
+            System.out.println(average);
+        }
+        else {
+            average = average / index;
+            System.out.println(average);
+        }
+
+    }
+}
